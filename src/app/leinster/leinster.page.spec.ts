a07f47eb0d5de78a16f900046792274befaf5ebd
killianosuilleabhain <killianosuilleabhain>
2021-03-24 16:25:19 +0000

    lab complete

--- src/app/leinster/leinster.page.spec.ts
+++ src/app/leinster/leinster.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { LeinsterPage } from './leinster.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('LeinsterPage', () => {
+  let component: LeinsterPage;
+  let fixture: ComponentFixture<LeinsterPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ LeinsterPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(LeinsterPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
